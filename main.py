967bff06e7f0155b6ed80842af61b67eeb7f5a91
jvkimmenade <126709486+jvkimmenade@users.noreply.github.com>
2023-05-03 21:50:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
-# def import_interval_set(filename="sample_set1.txt"):
-
-filename="sample_set1.txt"
-f = open(filename, "r")
-interval_set = []
-
-for line in f:
-    interval_list = line.strip()
-    interval_list = interval_list.replace("[", "").replace("]", "").split(",")
-    # interval_list = list(map(int, interval_list))
-    # interval_list = [interval_list[i:i + 2] for i in range(0, len(interval_list), 2)]
-    # interval_set.append(interval_list)
-f.close()
-    # return interval_set
+set1_path = "sample_set1.txt"
+set2_path = "sample_set2.txt"
 
 
-# interval1 = import_interval_set("sample_set1.txt")[0][0]
-# interval2 = import_interval_set("sample_set2.txt")[0][0]
+def read_intervals(filepath):
+    with open(filepath, 'r') as f:
+        intervals = [line.strip() for line in f.readlines()]
+    return [tuple(int(bound.strip('[], ')) for bound in interval.split(',')) for interval in intervals]
 
-# def check_overlap_between_intervals(interval1,interval2):
+
+list1 = read_intervals(set1_path)[0]
+list2 = read_intervals(set2_path)[0]
+
+def temp_func(line1, line2):
+    intervals1 = [tuple(line1[i:i+2]) for i in range(0, len(line1), 2)]
+    intervals2 = [tuple(line2[i:i+2]) for i in range(0, len(line2), 2)]
+    return intervals1, intervals2
+
+int1, int2 = temp_func(list1,list2)
+print(int1)
+print(int2)
+
+
+def has_overlap(I, L):
+    for interval in L:
+        if I[0] <= interval[1] and interval[0] <= I[1]:
+            return 1
+    return 0
+
+
+def count_overlaps(interval_list1, interval_list2):
+    i, j = 0, 0
+    count = 0
+
+    while i < len(interval_list1) and j < len(interval_list2):
+        if interval_list1[i][1] < interval_list2[j][0]:
+            i += 1
+        elif interval_list2[j][1] < interval_list1[i][0]:
+            j += 1
+        else:
+            count += has_overlap(interval_list1[i], interval_list2)
+            i += 1
+
+    return count
+
